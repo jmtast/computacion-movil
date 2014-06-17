61343de61aa85c2b88d965add5e3ec8cca7d5f6d
prago <p4blo.r@gmail.com>
2014-06-17 01:07:58 -0300

    cambios para gps en el main

--- app/workspace/example/Example/src/com/hw/example/MainActivity.java
+++ app/workspace/example/Example/src/com/hw/example/MainActivity.java
@@ -1,26 +1,63 @@
 package com.hw.example;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
 
-	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
-	
-	public final static String NONE = "com.example.myfirstapp.NONE";
-	public final static String TAXI = "com.example.myfirstapp.TAXI";
-	public final static String PASSENGER = "com.example.myfirstapp.PASSENGER";
-	
+public class MainActivity extends ActionBarActivity implements
+    GooglePlayServicesClient.ConnectionCallbacks,
+    GooglePlayServicesClient.OnConnectionFailedListener{
+
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    
+    public final static String NONE = "com.example.myfirstapp.NONNE";
+    public final static String TAXI = "com.example.myfirstapp.TAXI";
+    public final static String PASSENGER = "com.example.myfirstapp.PASSENGER";
+    
+    private LocationClient mLocationClient;
+    
+    /*
+     * Called when the Activity becomes visible .
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Connect the client.
+        mLocationClient.connect();
+    }
+    
+    /*
+     * Called when the Activity is no longer visible.
+     */
+    @Override
+    protected void onStop() {
+        // Disconnecting the client invalidates it.
+        mLocationClient.disconnect();
+        super.onStop();
+    }
+    
+    
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +68,12 @@ public class MainActivity extends ActionBarActivity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+        
+        /*
+         * Create a new location client, using the enclosing class to
+         * handle callbackss.
+         */
+        mLocationClient = new LocationClient(this, this, this);
         
         SharedPreferences sharedPref = getSharedPreferences(getString(R.string.user_type_preference_key), Context.MODE_PRIVATE);
         
@@ -43,16 +86,16 @@ public class MainActivity extends ActionBarActivity {
         String userType = sharedPref.getString(getString(R.string.user_type), NONE);
         
         if(NONE.equals(userType)){
-        	Intent intent = new Intent(this, UserTypeChoosingActivity.class);
-        	startActivity(intent);
+            Intent intent = new Intent(this, UserTypeChoosingActivity.class);
+            startActivity(intent);
         }else if(TAXI.equals(userType)){
-        	Intent intent = new Intent(this, DisplayMessageActivity.class);
-        	intent.putExtra(EXTRA_MESSAGE, "taxi");
-        	startActivity(intent);
+            Intent intent = new Intent(this, TaxiConfigActivity.class);
+            intent.putExtra(EXTRA_MESSAGE, "taxi");
+            startActivity(intent);
         }else if(PASSENGER.equals(userType)){
-        	Intent intent = new Intent(this, DisplayMessageActivity.class);
-        	intent.putExtra(EXTRA_MESSAGE, "passenger");
-        	startActivity(intent);
+            Intent intent = new Intent(this, DisplayMessageActivity.class);
+            intent.putExtra(EXTRA_MESSAGE, "passenger");
+            startActivity(intent);
         }
     }
 
@@ -79,11 +122,11 @@ public class MainActivity extends ActionBarActivity {
     
     /** Called when the user clicks the Send button */
     public void sendMessage(View view) {
-    	Intent intent = new Intent(this, DisplayMessageActivity.class);
-    	EditText editText = (EditText) findViewById(R.id.edit_message);
-    	String message = editText.getText().toString();
-    	intent.putExtra(EXTRA_MESSAGE, message);
-    	startActivity(intent);	
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);  
     }
 
     /**
@@ -102,4 +145,167 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 
+    
+    
+    //----------------------------- Google play services -----------------------------------
+    // Error handling
+    
+ // Global constants
+    /*
+     * Define a request code to send to Google Play services
+     * This code is returned in Activity.onActivityResult
+     */
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+    
+    // Define a DialogFragment that displays the error dialog
+    public static class ErrorDialogFragment extends DialogFragment {
+        // Global field to contain the error dialog
+        private Dialog mDialog;
+        // Default constructor. Sets the dialog field to null
+        public ErrorDialogFragment() {
+            super();
+            mDialog = null;
+        }
+        // Set the dialog to display
+        public void setDialog(Dialog dialog) {
+            mDialog = dialog;
+        }
+        // Return a Dialog to the DialogFragment.
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            return mDialog;
+        }
+    }
+    
+    /*
+     * Handle results returned to the FragmentActivity
+     * by Google Play services
+     */
+    @Override
+    protected void onActivityResult(
+            int requestCode, int resultCode, Intent data) {
+        // Decide what to do based on the original request code
+        switch (requestCode) {
+//            ...
+            case CONNECTION_FAILURE_RESOLUTION_REQUEST :
+            /*
+             * If the result code is Activity.RESULT_OK, try
+             * to connect again
+             */
+                switch (resultCode) {
+                    case Activity.RESULT_OK :
+                    /*
+                     * Try the request again
+                     */
+//                    ...
+                    break;
+                }
+//            ...
+        }
+     }
+    
+    private boolean servicesConnected() {
+        // Check that Google Play services is available
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        
+        // If Google Play services is available
+        if (ConnectionResult.SUCCESS == resultCode) {
+            // In debug mode, log the status
+            Log.d("Location Updates", "Google Play services is available.");
+            // Continue
+            return true;
+        // Google Play services was not available for some reason
+        } else {
+            showErrorDialog(1);
+            
+            return false;
+        }
+    }
+    
+    
+    // ... ???
+    
+    private void showErrorDialog(int errorCode){
+        // Get the error code
+//        int errorCode = 1; // connectionResult.getErrorCode(); // WTF ???????????????????
+        
+        // Get the error dialog from Google Play services
+        Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(
+                errorCode,
+                this,
+                CONNECTION_FAILURE_RESOLUTION_REQUEST);
+
+        // If Google Play services can provide an error dialog
+        if (errorDialog != null) {
+            // Create a new DialogFragment for the error dialog
+            ErrorDialogFragment errorFragment =
+                    new ErrorDialogFragment();
+            // Set the dialog in the DialogFragment
+            errorFragment.setDialog(errorDialog);
+            // Show the error dialog in the DialogFragment
+            errorFragment.show(getSupportFragmentManager(),
+                    "Location Updates");
+        }
+    }
+    
+    // Implementing Interfaces
+    
+    /*
+     * Called by Location Services when the request to connect the
+     * client finishes successfully. At this point, you can
+     * request the current location or start periodic updates
+     */
+    @Override
+    public void onConnected(Bundle dataBundle) {
+        // Display the connection status
+        Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
+
+    }
+    
+    /*
+     * Called by Location Services if the connection to the
+     * location client drops because of an error.
+     */
+    @Override
+    public void onDisconnected() {
+        // Display the connection status
+        Toast.makeText(this, "Disconnected. Please re-connect.",
+                Toast.LENGTH_SHORT).show();
+    }
+    
+    /*
+     * Called by Location Services if the attempt to
+     * Location Services fails.
+     */
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        /*
+         * Google Play services can resolve some errors it detects.
+         * If the error has a resolution, try sending an Intent to
+         * start a Google Play services activity that can resolve
+         * error.
+         */
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(
+                        this,
+                        CONNECTION_FAILURE_RESOLUTION_REQUEST);
+                /*
+                 * Thrown if Google Play services canceled the original
+                 * PendingIntent
+                 */
+            } catch (IntentSender.SendIntentException e) {
+                // Log the error
+                e.printStackTrace();
+            }
+        } else {
+            /*
+             * If no resolution is available, display a dialog to the
+             * user with the error.
+             */
+            showErrorDialog(connectionResult.getErrorCode());
+        }
+    }
+    
 }
